9cf907789febbda08a82e4727947c48020a67fb3
Kalob Taulien <kalob.taulien@gmail.com>
2024-06-10 13:03:11 -0600

    Model validation for min/max text lengths

--- comments/models.py
+++ comments/models.py
@@ -1,5 +1,21 @@
 from django.db import models
 from django.utils.translation import gettext as _
+from django.core.exceptions import ValidationError
+
+
+# CLEANUP:
+# Move this function utils.py or a new file called validators.py
+def validate_text_length(value):
+    # TODO:
+    # - [ ] Look into better minimum and maximum numbers. 50 and 5000 are arbitrary based on zero data.
+    # - [ ] Apply better error messages
+    # - [ ] Consider allowing HTML in comments using a wysiwyg editor. Needs bleaching for security, though.
+    #       Security first. This would great for user engagement and adding emphasis to conversations,
+    #       Unfortunate I suspect I'll run out of time to implement this.
+    if len(value) < 50:
+        raise ValidationError('Text is too short. Minimum length is 50 characters.')
+    elif len(value) > 5000:
+        raise ValidationError('Text is too long. Maximum length is 5000 characters.')
 
 
 # Ratings are stored as integers. Small spacing between each number is used to allow for future expansion
@@ -37,7 +53,9 @@ class CommonFields(models.Model):
         choices=AGREEMENT_CHOICES,
         default=0,
     )
-    text = models.TextField()  # TODO: Add validation for text length
+    text = models.TextField(
+        validators=[validate_text_length],
+    )
 
     class Meta:
         abstract = True
