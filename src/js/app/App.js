014a609bd6fca41915b1da5a7fe42506cbddef66
Kalob Taulien <kalob.taulien@gmail.com>
2024-06-10 13:30:49 -0600

    Test fetching data from the backend

--- src/js/app/App.js
+++ src/js/app/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 function App(props) {
+    const [originalCommentData, setOriginalCommentData] = useState([]);
+
+    useEffect(() => {
+      fetch('http://localhost:8000/api/v1/comments/') // Versioned API URL
+        .then(response => response.json())
+        .then(data => {
+          // Debating whether comments and replies should be separate or not. For now, I'm going with together.
+          setOriginalCommentData(data)
+        });
+    }, []);
+
     return (
       <div>
-        Did I do a thing? I think I did a thing.
+        {originalCommentData && (
+          <>
+            {originalCommentData.map((comment, index) => (
+              <div>{comment.text} by {comment.user_name}</div>
+            ))}
+          </>
+        )}
       </div>
     );
   }
