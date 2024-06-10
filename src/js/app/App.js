bfc1ebd4d7e5c86b3eb32ce9f11ee6605ab35b6d
Kalob Taulien <kalob.taulien@gmail.com>
2024-06-10 13:08:02 -0600

    React seems to be working

---
+++ src/js/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App(props) {
+    return (
+      <div>
+        Did I do a thing? I think I did a thing.
+      </div>
+    );
+  }
+
+  export default App;
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
-console.log("Is this thing on? :P")
+import React from 'react';
+import App from './app/App';
+import { createRoot } from 'react-dom/client';
+
+// Look for #app in the DOM. If it exists, render the App component.
+const container = document.getElementById('app') || false;
+if(container) {
+  const root = createRoot(container);
+  root.render(<App />);
+}
