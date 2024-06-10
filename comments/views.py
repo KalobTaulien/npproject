4d9ffb6fd4aef5284fe4c2f9b2b7df8c50dceb7b
Kalob Taulien <kalob.taulien@gmail.com>
2024-06-10 14:13:05 -0600

    Reply POST requests - no frontend support yet

--- comments/views.py
+++ comments/views.py
@@ -1,8 +1,10 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.parsers import JSONParser
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from .models import Comment, Reply
+from .forms import ReplyForm
 
 
 class CommentView(APIView):
@@ -32,7 +34,27 @@ class ReplyView(APIView):
         return Reply.objects.none()
 
     def post(self, request, *args, **kwargs):
-        # Let's test this before assuming the next batch of work gets built on top of this.
-        return Response({
-            "hello": "world"
-        }, status=200)
+        comment = get_object_or_404(Comment, pk=request.data.get("comment_id"))
+
+        # TODO:
+        # - [ ] Add GPT Validation for constructive conversations
+        # - [ ] Time permitting, GPT could also verify if the reply is inline with the emoji/agreement value the user selects.
+
+        data = {
+            'comment': request.data.get("comment_id"),
+            'text': request.data.get("reply"),
+        }
+
+        # Let Django and the thousands of people before us handle the form validation.
+        # Somethings don't need to be reinvented. That's just my 2 cents.
+        form = ReplyForm(data)
+
+        if form.is_valid():
+            reply = form.save(commit=False)
+            reply.user_id = 1 # request.user  # TODO: Implement user authentication via JWT. Will be simple with `djangorestframework-simplejwt`. Time permitting, this would be ideal.
+            reply.save()
+            return Response(reply.as_api())
+        else:
+            print("Error, Will Robinson!")
+            return Response({"error": form.errors}, status=400)
+
