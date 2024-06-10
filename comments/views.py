e20fa523f2eaeefc891d3504026abcd91f2b842b
Kalob Taulien <kalob.taulien@gmail.com>
2024-06-10 14:22:02 -0600

    GPT api function

--- comments/views.py
+++ comments/views.py
@@ -1,4 +1,6 @@
+from openai import OpenAI
 from django.shortcuts import get_object_or_404
+from django.conf import settings
 from rest_framework.parsers import JSONParser
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -7,12 +9,68 @@ from .models import Comment, Reply
 from .forms import ReplyForm
 
 
+client = OpenAI(
+    # This is the default and can be omitted
+    api_key="",
+)
+
+# TODO:
+# - [ ] Add this function to utils.py or something similar
+def get_gpt_response(comment: str, reply: str, model='gpt-3.5-turbo') -> str:
+    """
+    Get a string response from OpenAI's GPT-3.5 API (default). Swappable for other models.
+
+    Ideally, GPT will return a response and a suggestion in the form of: "response::suggestion". Example:
+    ```
+    yes::Being able to disagree while sharing your opinion in a healthy way is important to help others learn from you.
+    ```
+    or
+    ```
+    no::It's OK to feel angry or overly excited, but it's equally important to express yourself in a way that's respectful and constructive.
+    ```
+
+    The :: is split to get a binary `yes/no` response, plus a text based suggestion.
+    """
+
+    # TODO: It's much more efficient if we fine tuned a gpt to do this task rather than
+    #       prompting it with the same large token prompt over and over.
+    messages = [{
+        "role": "system",
+        "content": """
+        Promp required, soon. I'm guessing I won't have time to fine tune a custom GPT for this, but ideally we would do that instead of creating a long prompt that abuses the token system.
+        """
+    }]
+
+    # I have an idea for a single response prompt thats both information and helpful.
+    messages.append({
+        "role": "user",
+        "content": f"Comment: {comment}"
+    })
+    messages.append({
+        "role": "user",
+        "content": f"Reply: {reply}"
+    })
+
+    # TODO: Wrap in a try/catch block to catch any errors from the API
+    #       See: https://platform.openai.com/docs/guides/error-codes/api-errors
+    chat = client.chat.completions.create(
+        messages=messages,
+        model=model,
+    )
+
+    # response = yes|no
+    # suggestion = text suggestion from gpt
+    response, suggestion = chat.choices[0].message.content.split('::', 1)
+
+    return response, suggestion
+
+
 class CommentView(APIView):
     parser_classes = [JSONParser]
     http_method_names = ["get", "post"]
 
     def get_queryset(self):
-        # TODO: Implement pagination for the comments.
+        # TODO: Implement pagination for the comments. DRF is best for that.
         # TODO: Implement a filter for the comments. Namely for the specific webpage this is being used on.
         return Comment.objects.all().order_by("-date_posted")
 
@@ -20,10 +78,6 @@ class CommentView(APIView):
         comments = self.get_queryset()
         comments = [comment.as_api() for comment in comments]
         return Response(comments)
-
-    # def post(self, request, *args, **kwargs):
-    #     # We _could_ support posting to /comments/, but for now this is disabled.
-    #     return super().post(request, *args, **kwargs)
 
 
 class ReplyView(APIView):
@@ -36,9 +90,14 @@ class ReplyView(APIView):
     def post(self, request, *args, **kwargs):
         comment = get_object_or_404(Comment, pk=request.data.get("comment_id"))
 
-        # TODO:
-        # - [ ] Add GPT Validation for constructive conversations
-        # - [ ] Time permitting, GPT could also verify if the reply is inline with the emoji/agreement value the user selects.
+
+        if settings.OPENAI_API_KEY:
+            # TODO:
+            # - [ ] Time permitting, GPT could also verify if the reply is inline with the emoji/agreement value the user selects.
+            response, suggestion = get_gpt_response(comment.text, request.data.get("reply"))
+
+            if response.lower() == "no":
+                return Response(suggestion, status=400)
 
         data = {
             'comment': request.data.get("comment_id"),
